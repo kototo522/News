82e51c87ebb9068ef074377c614c1e36f17d3ecd
kototo522 <huyejiben@gmail.com>
2022-07-20 08:46:43 +0900

    ファイル名の変更

--- app/src/main/java/com/example/newsapp/FavoriteFragment.kt
+++ app/src/main/java/com/example/newsapp/FavoriteFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FavoliteFragment.newInstance] factory method to
+ * Use the [FavoriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FavoliteFragment : Fragment() {
+class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class FavoliteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favolite, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class FavoliteFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FavoliteFragment().apply {
+            FavoriteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
