e014f8ef7257bbb95911bbdfbcf5ba9272af6fd1
kototo522 <huyejiben@gmail.com>
2023-01-06 16:17:36 +0900

    データを持って来れません！！

--- app/src/main/java/com/example/newsapp/TabFragment.kt
+++ app/src/main/java/com/example/newsapp/TabFragment.kt
@@ -8,11 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import coil.load
+import com.example.newsapp.data.datasource.LifehackerRemoteDataSourceImpl
+import com.example.newsapp.data.datasource.model.LifehackerContentDataSourceModel
 import com.example.newsapp.databinding.FragmentSearchBinding
 import com.example.newsapp.databinding.FragmentTab01Binding
 import com.example.newsapp.databinding.FragmentTab02Binding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import java.lang.reflect.Array.set
 
 class Tab01Fragment: Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -21,9 +24,9 @@ class Tab01Fragment: Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val myDataset = Datasource()
+        val dataset = LifehackerRemoteDataSourceImpl()
         val recyclerView: RecyclerView = view.findViewById(R.id.Recycler_view)
-        recyclerView.adapter = ItemAdapter(requireContext(), myDataset.loadTitles(), myDataset.loadTexts(), myDataset.loadImages())
+        recyclerView.adapter = ItemAdapter(requireContext(), dataset.get(LifehackerContentDataSourceModel->title), Datasource().loadTexts(), Datasource().loadImages())
         recyclerView.setHasFixedSize(true)
     }
 }
